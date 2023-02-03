52e70d9cc5a1012c3836537aa1ac85a20e86a5bc
Taro Hanamura <m@hanamurataro.com>
2023-02-03 13:36:51 +0900

    Add a mastodon link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
+import '../styles/globals.css'
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Typefest</title>
+        <link rel="me" href="https://mastodon.social/@typefest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
